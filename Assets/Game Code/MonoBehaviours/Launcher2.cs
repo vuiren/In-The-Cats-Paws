19db50b9a7bc3a76f4849547a03bb49e2181c62c
vuiren <mr.vuiren@gmail.com>
2021-12-25 10:18:57 +0500

    Переделал интерфейс лаунчера

---
+++ Assets/Game Code/MonoBehaviours/Launcher2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game_Code.MonoBehaviours
+{
+    public class Launcher2 : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private string playerName, roomName;
+        [SerializeField] private Image statusImage;
+        [SerializeField] private Button joinRoomButton;
+        [SerializeField] private GameObject joinRoomUI, joinArenaUI;
+        
+        private const string GameVersion = "1";
+
+        
+        private void Start()
+        {
+            joinArenaUI.SetActive(false);
+            statusImage.color = Color.red;
+            joinRoomButton.interactable = false;
+            
+            ConnectToPhoton();
+        }
+        
+        private static void ConnectToPhoton()
+        {
+            if (PhotonNetwork.IsConnected) return;
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = GameVersion;
+        }
+
+        public override void OnConnected()
+        {
+            base.OnConnected();
+            
+            statusImage.color = Color.green;
+            joinRoomButton.interactable = true;
+        }
+        
+        public void JoinRoom()
+        {
+            if (!PhotonNetwork.IsConnected) return;
+            statusImage.color = Color.yellow;
+        
+            PhotonNetwork.LocalPlayer.NickName = playerName; //1
+            Debug.Log("PhotonNetwork.IsConnected! | Trying to Create/Join Room " +
+                      roomName);
+            var roomOptions = new RoomOptions() { MaxPlayers = 2 }; //2
+            var typedLobby = new TypedLobby(roomName, LobbyType.Default); //3
+            PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby); //4
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Room joined");
+
+            base.OnJoinedRoom();
+            
+                        
+            joinRoomUI.SetActive(false);
+            joinArenaUI.SetActive(true);
+        }
+
+        public override void OnLeftRoom()
+        {
+            base.OnLeftRoom();
+            
+            joinRoomUI.SetActive(true);
+            joinArenaUI.SetActive(false);
+        }
+
+        public void SetRoomName(string roomName)
+        {
+            this.roomName = roomName;
+        }
+
+        public void SetPlayerName(string playerName)
+        {
+            this.playerName = playerName;
+        }
+    }
+}
